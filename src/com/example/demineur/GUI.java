d648d4b33e5f732c785b46790d6411403a59d95e
DEMEULEMEESTER <stephane.demeulemeester@cgi.com>
2019-05-24 23:42:20 +0200

    first commit

---
+++ src/com/example/demineur/GUI.java
@@ -0,0 +1,11 @@
+package com.example.demineur;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame{
+	public GUI () {
+		this.setTitle("Démineur");
+		this.setSize(1286,829);
+		this.setVisible(true);
+	}
+}
---
+++ src/com/example/demineur/Main.java
@@ -0,0 +1,18 @@
+package com.example.demineur;
+
+public class Main implements Runnable{
+	
+	GUI gui = new GUI();
+
+	public static void main (String[]args) {
+		new Thread(new Main()).start();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			gui.repaint();
+		}
+	}
+	
+}
