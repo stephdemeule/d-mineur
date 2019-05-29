6ae39cb217321c3578a420ebda7ecf7924c15e64
stephane.demeulemees <stephane.demeulemees@FR-L4228378.groupinfra.com>
2019-05-29 19:08:40 +0200

    change

--- src/com/example/demineur/GUI.java
+++ src/com/example/demineur/GUI.java
@@ -1,5 +1,8 @@
 package com.example.demineur;
 
+import java.util.Iterator;
+import java.util.Random;
+
 import javax.swing.JFrame;
 /**
  * Cette classe crée le cadre du jeu
@@ -8,12 +11,28 @@ import javax.swing.JFrame;
  */
 
 public class GUI extends JFrame{
+	
+	Random rand = new Random();
+	
+	static int [][] mines = new int [16][9];
+	
 	public GUI () {
 		this.setTitle("Démineur");
 		this.setSize(1286,829);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setResizable(false);
+		
+		for (int i = 0; i < 16; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (rand.nextInt(100) <20){
+					mines[i][j]=1;
+				}
+				else {
+					mines[i][j]=0;
+				}
+			}
+		}
 		
 		Board board = new Board();
 		this.setContentPane(board);
